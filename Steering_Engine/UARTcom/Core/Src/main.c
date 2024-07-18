886e852eeea39bd3581274b8c8b2ff09271b9d8c
Liu Jiashu <kim_liujs@foxmail.com>
2024-07-19 00:25:31 +0800

    finally motor can move. a up date of PWM

--- Steering_Engine/UARTcom/Core/Src/main.c
+++ Steering_Engine/UARTcom/Core/Src/main.c
@@ -59,9 +59,9 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -89,8 +89,9 @@ int main(void)
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
   uint16_t ledrpwmval = 0;
-  uint8_t dir = 1;
-
+  uint16_t motorpwm = 3000;
+  uint8_t dir = 0;
+  //__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 2000);这句是试验，只给一个占空比1：9的脉冲能不能动，结果是不行
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -108,30 +109,32 @@ int main(void)
       dir = 0;
     if (ledrpwmval == 0)
       dir = 1;*/
-    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 2000);
-    delay_ms(40);
-    /*__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 1500);
-    delay_ms(40);
-    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 2000);
-    delay_ms(40);
-    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 2500);
-    delay_ms(40);*/
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, motorpwm);//必须得是变量，常量动不了，而且下面的几个量和舵机的运动位置都没有关系
+    delay_ms(20);
+    if (dir)
+      motorpwm += 200;
+    else
+      motorpwm -= 200;
+    if (motorpwm > 3000)
+      dir = 0;
+    if (motorpwm < 1500)
+      dir = 1;
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -145,9 +148,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -164,9 +166,9 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -178,14 +180,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
